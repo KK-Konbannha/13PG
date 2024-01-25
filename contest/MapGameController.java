280d37584a8a70c8d05709f39b153a6dea25d9a0
KK-Konbannha <katsuk.618@gmail.com>
2024-01-26 01:45:28 +0900

    fix print action

--- contest/MapGameController.java
+++ contest/MapGameController.java
@@ -58,7 +58,7 @@ public class MapGameController implements Initializable {
 
     // Operations for going the cat up
     public void upButtonAction() {
-        printAction("UP");
+        printAction(MoveChara.TYPE_UP);
         chara.setCharaDirection(MoveChara.TYPE_UP);
         chara.move(MoveChara.TYPE_UP);
         drawMap();
@@ -66,7 +66,7 @@ public class MapGameController implements Initializable {
 
     // Operations for going the cat down
     public void downButtonAction() {
-        printAction("DOWN");
+        printAction(MoveChara.TYPE_DOWN);
         chara.setCharaDirection(MoveChara.TYPE_DOWN);
         chara.move(MoveChara.TYPE_DOWN);
         drawMap();
@@ -74,7 +74,7 @@ public class MapGameController implements Initializable {
 
     // Operations for going the cat right
     public void leftButtonAction() {
-        printAction("LEFT");
+        printAction(MoveChara.TYPE_LEFT);
         chara.setCharaDirection(MoveChara.TYPE_LEFT);
         chara.move(MoveChara.TYPE_LEFT);
         drawMap();
@@ -82,26 +82,22 @@ public class MapGameController implements Initializable {
 
     // Operations for going the cat right
     public void rightButtonAction() {
-        printAction("RIGHT");
+        printAction(MoveChara.TYPE_RIGHT);
         chara.setCharaDirection(MoveChara.TYPE_RIGHT);
         chara.move(MoveChara.TYPE_RIGHT);
         drawMap();
     }
 
-    
-
-    
-
     @FXML
     public void func1ButtonAction(ActionEvent event) {
         try {
-            //追加
+            // 追加
             lb_0.setText("");
             System.out.println("func1");
             StageDB.getMainStage().hide();
             StageDB.getMainSound().stop();
             StageDB.getGameOverStage().show();
-            StageDB.getGameOverSound().play();//ここ
+            StageDB.getGameOverSound().play();// ここ
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
@@ -109,8 +105,8 @@ public class MapGameController implements Initializable {
 
     @FXML
     public void func2ButtonAction(ActionEvent event) {
-        //追加
-        mapData.setBlackFlag(0);  //暗転オフ
+        // 追加
+        mapData.setBlackFlag(0); // 暗転オフ
         lb_0.setText("");
         mapData = new MapData(21, 15);
         chara = new MoveChara(1, 1, mapData);
@@ -121,40 +117,47 @@ public class MapGameController implements Initializable {
         drawMap();
         StageDB.getMainSound().play();
         System.out.println("func2: new map");
-        
-        
+
     }
 
-    //追加
-    //時間制限表示用のラベルを取得
+    // 追加
+    // 時間制限表示用のラベルを取得
     @FXML
     private Label lb_0;
+
     @FXML
     public void func3ButtonAction(ActionEvent event) {
-        //追加
-        //試しで制限時間の機能を実行
+        // 追加
+        // 試しで制限時間の機能を実行
         MapGame.callTask(lb_0, mapData, chara);
     }
 
     @FXML
     public void func4ButtonAction(ActionEvent event) {
         System.out.println("func4: 暗転");
-        for(int bbx = 0; bbx < 21; bbx++){
-                for(int bby = 0; bby < 15; bby++){
-                    if(mapData.getMap(bbx, bby) == MapData.TYPE_SPACE || mapData.getMap(bbx, bby) == MapData.TYPE_WALL){
-                        mapData.setImageView(bbx, bby, MapData.TYPE_BLACK); //壁と床をすべて黒にする
-                    }
+        for (int bbx = 0; bbx < 21; bbx++) {
+            for (int bby = 0; bby < 15; bby++) {
+                if (mapData.getMap(bbx, bby) == MapData.TYPE_SPACE || mapData.getMap(bbx, bby) == MapData.TYPE_WALL) {
+                    mapData.setImageView(bbx, bby, MapData.TYPE_BLACK); // 壁と床をすべて黒にする
                 }
             }
-        mapData.setBlackFlag(1); //暗転オン
-        
-        
-        
-        
+        }
+        mapData.setBlackFlag(1); // 暗転オン
+
     }
 
     // Print actions of user inputs
-    public void printAction(String actionString) {
+    public void printAction(int type) {
+        String actionString = "";
+        if (type == MoveChara.TYPE_UP) {
+            actionString = "UP";
+        } else if (type == MoveChara.TYPE_DOWN) {
+            actionString = "DOWN";
+        } else if (type == MoveChara.TYPE_LEFT) {
+            actionString = "LEFT";
+        } else if (type == MoveChara.TYPE_RIGHT) {
+            actionString = "RIGHT";
+        }
         System.out.println("Action: " + actionString);
     }
 
